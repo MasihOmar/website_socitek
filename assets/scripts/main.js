b063b59acdc29332d5ac66c661ea249084ac698c
Masihullah Omar <127937324+MasihOmar@users.noreply.github.com>
2025-05-10 15:35:51 +0300

    Add files via upload

---
+++ assets/scripts/main.js
@@ -0,0 +1,13 @@
+registration-project
+├── src
+│   ├── pages
+│   │   ├── index.html
+│   │   ├── confirmation.html
+│   │   └── privacy.html
+│   ├── styles
+│   │   └── main.css
+│   └── assets
+│       └── scripts
+│           └── main.js
+├── package.json
+└── README.md
